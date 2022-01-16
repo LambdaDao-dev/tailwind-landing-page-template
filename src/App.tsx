9c716dd51467717d45a9227ed8d5979511203bf5
lambdadao.dev <lambdadao.dev@gmail.com>
2022-01-16 19:29:33 +0800

    feat: typescript support

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+
+import "./css/style.scss";
+
+import AOS from "aos";
+import { focusHandling } from "cruip-js-toolkit";
+
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ResetPassword from "./pages/ResetPassword";
+
+function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: "phone",
+      duration: 700,
+      easing: "ease-out-cubic",
+    });
+  });
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    if (!html) {
+      return;
+    }
+    html.style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    html.style.scrollBehavior = "";
+    // @ts-ignore
+    focusHandling("outline");
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/reset-password">
+          <ResetPassword />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
